70e0ebee9fa688916700bb5fba0c11f8d7551782
Facundo <facugastonbarral@gmail.com>
2022-08-10 14:57:37 -0300

    Agregado bootstrap y navbar

--- index.php
+++ index.php
@@ -1,19 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="./main.css">
     <title>Turismo Cafayate</title>
 </head>
+
 <body>
-    <div class="container">
-        <h1 class="text-center">Cafayate Web</h1>
-    </div>
+    <nav class="navbar navbar-expand-lg bg-light navbar-shadow">
+        <div class="container-fluid justify-content-around">
+            <a class="navbar-brand ms-5" href="#">
+                <img class="navbar-logo" src="./images/logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav w-100 justify-content-end me-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Turismo</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Comercios</a>
+                    </li>
+                    <li class="nav-item me-5">
+                        <a class="nav-link disabled">Novedades</a>
+                    </li>
+                    <li class="nav-item">
+                        <img class="rounded-circle" width="40" height="40" src="./images/perfil.png">
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js" integrity="sha384-ODmDIVzN+pFdexxHEHFBQH3/9/vQ9uori45z4JjnFsRydbmQbmL5t1tQ0culUzyK" crossorigin="anonymous"></script>
 </body>
+
 </html>
