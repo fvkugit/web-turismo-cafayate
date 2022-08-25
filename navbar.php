b0ad519e22cb65e0fa3ebc65c6bd0f9c6e96abfb
Facundo <facugastonbarral@gmail.com>
2022-08-25 12:11:50 -0300

    Creada navbar para usarla como componente

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<div class="wrap">
+    <header id="header">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <button id="primary-nav-button" type="button">Menu</button>
+                    <a href="inicio.html">
+                        <div class="logo">
+                            <img src="img/logo2.png" alt="Venue Logo">
+                        </div>
+                    </a>
+                    <nav id="primary-nav" class="dropdown cf">
+                        <ul class="dropdown menu">
+                            <li class='active'><a href="inicio.html">Inicio</a></li>
+
+                            <li><a href="comercios.html">Comercios</a></li>
+
+                            <li><a href="novedades.html">Novedades</a></li>
+
+                            <li><a href="turismo.html">Turismo</a></li>
+
+                            <li><a href="turismo.html">Cuenta</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+</div>
