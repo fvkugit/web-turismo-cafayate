cf3d77fac5cfd6c34c635e579f7b53d528240726
Facundo <facugastonbarral@gmail.com>
2022-09-13 11:42:51 -0300

    Agregada navbar como componente

--- result.php
+++ result.php
@@ -1,68 +1,43 @@
 <?php
 
 include "config.php"; #Include PHP DB
-$message = "Su registro fue completado con exito"
+// $message = "Su registro fue completado con exito"
 
 ?>
 
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Proyecto Cafayate Inicio</title>
-        
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="css/fontAwesome.css">
-        <link rel="stylesheet" href="css/hero-slider.css">
-        <link rel="stylesheet" href="css/owl-carousel.css">
-        <link rel="stylesheet" href="css/style.css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Proyecto Cafayate Inicio</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
-    </head>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/fontAwesome.css">
+    <link rel="stylesheet" href="css/hero-slider.css">
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
 
 <body>
-    <div class="wrap">
-        <header id="header">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <button id="primary-nav-button" type="button">Menu</button>
-                        <a href="inicio.html"><div class="logo">
-                            <img src="img/logo2.png" alt="Venue Logo">
-                        </div></a>
-                        <nav id="primary-nav" class="dropdown cf">
-                            <ul class="dropdown menu">
-                                <li class='active'><a href="inicio.html">Inicio</a></li>
+    <?php include_once 'navbar.php'; ?>
 
-                                <li><a href="comercios.html">Comercios</a></li>
-
-                                <li><a href="novedades.html">Novedades</a></li>
-
-                                <li><a href="turismo.html">Turismo</a></li>
-
-                                <li><a href="turismo.html">Cuenta</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
-        </header>
-    </div>
-      
     <section class="banner" id="top" style="background-image: url(img/Quebrada-de-cafayate.jpg);">
         <div class="container">
             <div class="row">
                 <div class="col-md-10 col-md-offset-1">
                     <div class="banner-caption">
                         <div class="line-dec"></div>
-                        <h2><?php echo($message) ?></h2>
+                        <h2><?php echo ($message) ?></h2>
                         <a href="./index.php">
                             <button class="orange btn btn-large pull-right">
                                 Continuar
@@ -76,17 +51,20 @@ $message = "Su registro fue completado con exito"
 
 
 
-        <div class="sub-footer">
-            <p>Copyright © 2022 Practica Profesionalizante II Di Benedetto - Barral</p>
-        </div>
+    <div class="sub-footer">
+        <p>Copyright © 2022 Practica Profesionalizante II Di Benedetto - Barral</p>
+    </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
-    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+    <script>
+        window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+    </script>
 
     <script src="js/vendor/bootstrap.min.js"></script>
-    
+
     <script src="js/datepicker.js"></script>
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
 </body>
+
 </html>
