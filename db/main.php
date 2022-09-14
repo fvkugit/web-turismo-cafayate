a0489ea746b6aa86bfe1fa02f7a49b826e78037e
Facundo <facugastonbarral@gmail.com>
2022-09-14 19:14:31 -0300

    Instanciado modelo de comercios

--- db/main.php
+++ db/main.php
@@ -6,5 +6,6 @@ $usuarios = new Usuarios("usuarios", $db_base);
 $solicitudes = new Solicitudes("solicitudes_comerciante", $db_base);
 $novedades = new Novedades("novedades", $db_base);
 $dpublica = new DPublica("datos_publicos", $db_base);
+$comercios = new Comercios("comercios", $db_base);
 
 ?>
