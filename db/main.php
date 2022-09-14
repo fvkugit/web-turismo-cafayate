789a853f6a690ff7d0dd99eccfd9dd02164ce6ed
Facundo <facugastonbarral@gmail.com>
2022-09-14 16:12:51 -0300

    Instanciada tabla de datos publicos y novedades

--- db/main.php
+++ db/main.php
@@ -4,5 +4,7 @@ require_once("./db/model.php");
 $db_base = new DBConexion();
 $usuarios = new Usuarios("usuarios", $db_base);
 $solicitudes = new Solicitudes("solicitudes_comerciante", $db_base);
+$novedades = new Novedades("novedades", $db_base);
+$dpublica = new DPublica("datos_publicos", $db_base);
 
 ?>
