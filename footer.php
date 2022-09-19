63a45e30782304abff24043b52f7c3cf4d8d319d
Facundo <facugastonbarral@gmail.com>
2022-09-19 19:06:28 -0300

    Creado footer como componente

---
+++ footer.php
@@ -0,0 +1,36 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="about-veno">
+                        <div class="logo">
+                            <img src="img/logo2.png" alt="Venue Logo">
+                        </div>
+                        <p>Copyright © 2022 - Proyecto Cafayate</p>
+                        
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="contact-info">
+                        <div class="footer-heading">
+                            <h4>Contacto</h4>
+                        </div>
+                        <p><i class="fa fa-map-marker"></i><a href="https://goo.gl/maps/nCDX4NmT6X7ogtHx8"><span>  Cafayate - Argentina</span></a></p>
+                        <p><i class="fa fa-phone"></i><span>  Telefono:</span><a href="https://wa.me/543410000000?text=Hola">+54.000-000-0000</a></p>
+                        <p><i class="fa fa-envelope"></i><span>  Correo:</span><a href="mailto:info@cafayate.com">info@cafayate.com</a></p>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contact-info">
+                        <div class="footer-heading">
+                            <h4>Nuestras Redes</h4>
+                        </div>
+                        <p><i class="fa fa-instagram"></i><a href="https://instagram.com"><span> Instagram</span></a></p>
+                        <p><i class="fa fa-youtube"></i><a href="https:youtube.com"> Youtube </a></p>
+                        <p><i class="fa fa-facebook"></i><a href="https:facebook.com"> Facebook</a></p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </footer>
