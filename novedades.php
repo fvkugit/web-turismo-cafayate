e1bec894fb2a5a70d8c8df9abd36b85e4972058e
Facundo <facugastonbarral@gmail.com>
2022-09-20 17:10:10 -0300

    Creado archivo php de listado novedades

---
+++ novedades.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Proyecto Cafayate Novedades</title>
+
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="css/fontAwesome.css">
+    <link rel="stylesheet" href="css/hero-slider.css">
+    <link rel="stylesheet" href="css/owl-carousel.css">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+
+    <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+</head>
+
+<body>
+
+
+    <?php include_once("./navbar.php"); ?>
+
+    <section class="banner banner-secondary" id="top" style="background-image: url(img/Quebrada-de-cafayate.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1">
+                    <div class="banner-caption">
+                        <div class="line-dec"></div>
+                        <h2>Novedades</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <main>
+        <section class="featured-places">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-9 col-md-8 col-xs-12">
+                        <div class="row">
+                            <div class="col-sm-6 col-xs-12">
+                                <div class="featured-item">
+                                    <div class="thumb">
+                                        <div class="thumb-img">
+                                            <img src="img/blog-1-720x480.jpg" alt="">
+                                        </div>
+
+                                        <div class="overlay-content">
+                                            <strong title="Posted on"><i class="fa fa-calendar"></i> 12/06/2020 10:30</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <strong title="Views"><i class="fa fa-map-marker"></i> 115</strong>
+                                        </div>
+                                    </div>
+
+                                    <div class="down-content">
+                                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit hic</h4>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim consectetur assumenda nam facere voluptatibus totam veritatis. </p>
+
+                                        <div class="text-button">
+                                            <a href="novevades-detalles.html">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-xs-12">
+                                <div class="featured-item">
+                                    <div class="thumb">
+                                        <div class="thumb-img">
+                                            <img src="img/blog-2-720x480.jpg" alt="">
+                                        </div>
+
+                                        <div class="overlay-content">
+
+                                            <strong title="Posted on"><i class="fa fa-calendar"></i> 12/06/2020 10:30</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <strong title="Views"><i class="fa fa-map-marker"></i> 115</strong>
+                                        </div>
+                                    </div>
+
+                                    <div class="down-content">
+                                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit hic</h4>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim consectetur assumenda nam facere voluptatibus totam veritatis. </p>
+
+                                        <div class="text-button">
+                                            <a href="novevades-detalles.html">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-xs-12">
+                                <div class="featured-item">
+                                    <div class="thumb">
+                                        <div class="thumb-img">
+                                            <img src="img/blog-3-720x480.jpg" alt="">
+                                        </div>
+
+                                        <div class="overlay-content">
+
+                                            <strong title="Posted on"><i class="fa fa-calendar"></i> 12/06/2020 10:30</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <strong title="Views"><i class="fa fa-map-marker"></i> 115</strong>
+                                        </div>
+                                    </div>
+
+                                    <div class="down-content">
+                                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit hic</h4>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim consectetur assumenda nam facere voluptatibus totam veritatis. </p>
+
+                                        <div class="text-button">
+                                            <a href="novevades-detalles.html">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-xs-12">
+                                <div class="featured-item">
+                                    <div class="thumb">
+                                        <div class="thumb-img">
+                                            <img src="img/blog-4-720x480.jpg" alt="">
+                                        </div>
+
+                                        <div class="overlay-content">
+
+                                            <strong title="Posted on"><i class="fa fa-calendar"></i> 12/06/2020 10:30</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <strong title="Views"><i class="fa fa-map-marker"></i> 115</strong>
+                                        </div>
+                                    </div>
+
+                                    <div class="down-content">
+                                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit hic</h4>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim consectetur assumenda nam facere voluptatibus totam veritatis. </p>
+
+                                        <div class="text-button">
+                                            <a href="novevades-detalles.html">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-xs-12">
+                                <div class="featured-item">
+                                    <div class="thumb">
+                                        <div class="thumb-img">
+                                            <img src="img/blog-5-720x480.jpg" alt="">
+                                        </div>
+
+                                        <div class="overlay-content">
+
+                                            <strong title="Posted on"><i class="fa fa-calendar"></i> 12/06/2020 10:30</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <strong title="Views"><i class="fa fa-map-marker"></i> 115</strong>
+                                        </div>
+                                    </div>
+
+                                    <div class="down-content">
+                                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit hic</h4>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim consectetur assumenda nam facere voluptatibus totam veritatis. </p>
+
+                                        <div class="text-button">
+                                            <a href="novevades-detalles.html">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-sm-6 col-xs-12">
+                                <div class="featured-item">
+                                    <div class="thumb">
+                                        <div class="thumb-img">
+                                            <img src="img/blog-6-720x480.jpg" alt="">
+                                        </div>
+
+                                        <div class="overlay-content">
+
+                                            <strong title="Posted on"><i class="fa fa-calendar"></i> 12/06/2020 10:30</strong> &nbsp;&nbsp;&nbsp;&nbsp;
+                                            <strong title="Views"><i class="fa fa-map-marker"></i> 115</strong>
+                                        </div>
+                                    </div>
+
+                                    <div class="down-content">
+                                        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit hic</h4>
+
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim consectetur assumenda nam facere voluptatibus totam veritatis. </p>
+
+                                        <div class="text-button">
+                                            <a href="novevades-detalles.html">Read More</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-4 col-xs-12">
+                        <div class="form-group">
+                            <h4>Busqueda de Novedades</h4>
+                        </div>
+                        <br>
+
+                        <div class="form-group">
+                            <h4>Novedades mas importantes</h4>
+                        </div>
+
+                        <p><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quae animi sint.</a></p>
+                        <p><a href="#">Non, magni, sequi. Explicabo illum quas debitis ut hic possimus, nesciunt mag!</a></p>
+                        <p><a href="#">Vatae expedita deleniti optio ex adipisci animi, iusto tempora. </a></p>
+                        <p><a href="#">Soluta non modi dolorem voluptates. Maiores est, molestiae dolor laborum.</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <?php include_once("./footer.php"); ?>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js" type="text/javascript"></script>
+    <script>
+        window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')
+    </script>
+
+    <script src="js/vendor/bootstrap.min.js"></script>
+
+    <script src="js/datepicker.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
