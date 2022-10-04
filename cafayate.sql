5ec490a17c361a3f0f88c260f0964da94b672c32
Facundo <facugastonbarral@gmail.com>
2022-10-04 18:29:54 -0300

    Agregado roles

--- cafayate.sql
+++ cafayate.sql
@@ -198,6 +198,7 @@ SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
 -- -----------------------------------------------------
 INSERT INTO novedades_categorias VALUES (1, "Noticias"), (2, "Eventos"), (3, "Convocatorias");
 INSERT INTO comercio_categoria VALUES (1, "Indumentaria"), (2, "Calzados"), (3, "Comida"), (4, "Bazar"), (5, "Otros");
+INSERT INTO roles VALUES (1, "Usuario"), (2, "Comerciante"), (3, "Admin");
 INSERT INTO usuarios VALUES
   (NULL, "Cuenta", "Admin", "3411111111", "admin@correo.com", "$2a$12$p88FfXAJ/ADqZSR.fHU2G.lulSLzy5fX01QslOCWqFhiv7P.637xS", "11111111", "3"), -- Password: admin1
   (NULL, "Cuenta", "Comerciante", "3411111111", "comerciante@correo.com", "$2a$12$l07csZFb1Dt68nuSlkHr3OVJA/PDvSkysPofvLWt0H1iRBX48Lo76", "11111111", "2"), -- Password: comerciante1
