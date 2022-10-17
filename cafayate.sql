2adef1023bb4b9d9cc9c2c44b795db175d60ae86
Facundo <facugastonbarral@gmail.com>
2022-10-17 12:25:17 -0300

    Default en fecha de novedades

--- cafayate.sql
+++ cafayate.sql
@@ -163,6 +163,8 @@ ENGINE = InnoDB
 AUTO_INCREMENT = 4
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
+ALTER TABLE `cafayate`.`novedades`
+CHANGE COLUMN `fecha` `fecha` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ;
 
 
 -- -----------------------------------------------------
